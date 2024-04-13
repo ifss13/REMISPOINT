d37f930a109637badf44d3fa7264983856fdd1e7
ifss13 <ianfrancosanchez@gmail.com>
2024-04-13 02:17:28 -0300

    13/4 cambios

--- main.py
+++ main.py
@@ -7,22 +7,15 @@ from kivy.core.window import Window
 #IMPORTACION KIVYMD
 
 from kivymd.app import MDApp
-from kivymd.uix.dialog import MDDialog
-from kivymd.uix.button import MDFlatButton
 
 
 #IMPORTACION BSD
 from screens.login import completo as login_completo
 from screens.conectar_db import conectar_db
 from screens.registro import nuevo_usuario
+from screens.principal import calculo
+from screens.principal import remis_confirmar
 
-#IMPORTACION DE GOOGLE MAPS
-import googlemaps
-
-#Leer API Key desde un archivo externo
-API = open("Google Maps Platform API Key.txt","r")
-APIKey = API.read()
-Maps = googlemaps.Client(key=APIKey)
 
 #Variables y otros
 Window.size = (300,580)
@@ -75,53 +68,17 @@ class LoginApp(MDApp):
         nuevo_usuario (screen_manager, self.miCursor, *args)
 
     def calculo(self):
-    #'FUNCION PARA EL BOTON DE LA SCREEN INICIO QUE CALCULA LAS DISTANCIAS'
-        salida= screen_manager.get_screen('inicio').ids['salida'].text
-        llegada= screen_manager.get_screen('inicio').ids['llegada'].text
-        if salida != "" and llegada != "":
-            Distancia=Maps.directions(salida,llegada)
-            DistanciaKM = (Distancia[0]['legs'][0]['distance']['text'])
-            HorasMinDuracion= (Distancia[0]['legs'][0]['duration']['text'])
-            screen_manager.get_screen('inicio').ids['l_salida'].text = f'{salida}'
-            screen_manager.get_screen('inicio').ids['l_llegada'].text = f'{llegada}'
-            screen_manager.get_screen('inicio').ids['l_distancia'].text = f'{DistanciaKM}'
-            screen_manager.get_screen('inicio').ids['l_duracion'].text = f'{HorasMinDuracion}'
+        #'FUNCION PARA EL BOTON DE LA SCREEN INICIO QUE CALCULA LAS DISTANCIAS'
+        calculo(screen_manager)
+
+    def remis_confirmar(self,*args):
+        remis_confirmar(screen_manager, *args)
 
     def inicio(self):
         screen_manager.current="inicio"
 
     def close(self, *args):
         self.dialog.dismiss()
-
-
-    def remis_confirmar(self,*args):
-            self.dialog = MDDialog(
-                    title = 'Confirmacion',
-                    text= '¿Confirma que quiere realizar el pedido?',
-                    buttons=[
-                        MDFlatButton(
-                            text="Si",
-                            text_color=self.theme_cls.accent_color,
-                            on_release=self.callback_yes
-                        ),
-                        MDFlatButton(
-                            text="No",
-                            text_color=self.theme_cls.accent_color,
-                            on_release=self.callback_no
-                        )
-                    ]
-                )
-
-            self.dialog.open()      
-
-    def callback_yes(self, *args):
-        # Actions to perform when "Si" is pressed
-        self.dialog.dismiss()
-        screen_manager.current = "base"  # Change to another screen
-
-    def callback_no(self, *args):
-        # Actions to perform when "No" is pressed
-        self.dialog.dismiss()  # Close the dialog
 
 if __name__ == '__main__':
     app = LoginApp()
